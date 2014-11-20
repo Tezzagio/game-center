e7dc5cd01d9fe30f9871d6e9e0aec8cab260c920
Łukasz Korecki <lukasz@coffeesounds.com>
2014-11-20 19:39:33 +0000

    super simple guessing game

--- app/controllers/secret_number_controller.rb
+++ app/controllers/secret_number_controller.rb
@@ -1,2 +1,11 @@
 class SecretNumberController < ApplicationController
+  def index
+  end
+
+  def guess
+    @number = params[:number].to_i
+    random_number = (rand()*10).to_i
+
+    @guessed =  @number == random_number
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'secret-number' => 'secret_number#index'
+  get 'secret-number/guess/:number' => 'secret_number#guess'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
